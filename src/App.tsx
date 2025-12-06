d9255aa697f8a40f1e49afafc4515a0631c738d1
Davi <davimf9702@gmail.com>
2025-12-06 18:34:40 -0300

    Added TermsOfService page.

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,8 @@ import RequestPasswordResetPage from './pages/RequestPasswordResetPage.tsx';
 import ResetPasswordPage from './pages/ResetPasswordPage.tsx';
 import UrlShortener from "./components/UrlShortener.tsx";
 import UrlRedirectPage from "./pages/UrlRedirectPage.tsx";
-import PrivacyPolicy from './pages/PrivacyPolicy.tsx'; // Importa a nova página
+import PrivacyPolicy from './pages/PrivacyPolicy.tsx';
+import TermsOfService from './pages/TermsOfService.tsx'; // Importa a nova página
 
 function App() {
   return (
@@ -58,8 +59,9 @@ function App() {
               <Route path="/reset-password" element={<ResetPasswordPage />} />
               <Route path="/shortener" element={<UrlShortener />} />
               <Route path="/r/:shortCode" element={<UrlRedirectPage />} />
-              {/* Rota para a Política de Privacidade */}
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              {/* Rota para os Termos de Serviço */}
+              <Route path="/terms-of-service" element={<TermsOfService />} />
             </Routes>
           </Layout>
         </Router>
